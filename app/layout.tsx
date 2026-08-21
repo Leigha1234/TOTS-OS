ac40ea45f8f344aac0e1cd6e65179dfe996a8c31
Leigha1234 <leigha0305@gmail.com>
2026-08-21 16:09:24 +0100

    add pixel again

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,137 @@ export default function RootLayout({
           rel="apple-touch-icon"
           href="/icon.png?v=4"
         />
+
+        {/* ==================================================
+            TIKTOK PIXEL
+        ================================================== */}
+
+        <script
+          id="tiktok-pixel"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject = t;
+
+                var ttq = w[t] = w[t] || [];
+
+                ttq.methods = [
+                  "page",
+                  "track",
+                  "identify",
+                  "instances",
+                  "debug",
+                  "on",
+                  "off",
+                  "once",
+                  "ready",
+                  "alias",
+                  "group",
+                  "enableCookie",
+                  "disableCookie",
+                  "holdConsent",
+                  "revokeConsent",
+                  "grantConsent"
+                ];
+
+                ttq.setAndDefer = function (target, method) {
+                  target[method] = function () {
+                    target.push(
+                      [method].concat(
+                        Array.prototype.slice.call(
+                          arguments,
+                          0
+                        )
+                      )
+                    );
+                  };
+                };
+
+                for (
+                  var i = 0;
+                  i < ttq.methods.length;
+                  i++
+                ) {
+                  ttq.setAndDefer(
+                    ttq,
+                    ttq.methods[i]
+                  );
+                }
+
+                ttq.instance = function (pixelId) {
+                  var instance =
+                    ttq._i[pixelId] || [];
+
+                  for (
+                    var i = 0;
+                    i < ttq.methods.length;
+                    i++
+                  ) {
+                    ttq.setAndDefer(
+                      instance,
+                      ttq.methods[i]
+                    );
+                  }
+
+                  return instance;
+                };
+
+                ttq.load = function (pixelId, options) {
+                  var url =
+                    "https://analytics.tiktok.com/i18n/pixel/events.js";
+
+                  ttq._i = ttq._i || {};
+                  ttq._i[pixelId] = [];
+                  ttq._i[pixelId]._u = url;
+
+                  ttq._t = ttq._t || {};
+                  ttq._t[pixelId] = +new Date();
+
+                  ttq._o = ttq._o || {};
+                  ttq._o[pixelId] =
+                    options || {};
+
+                  var script =
+                    document.createElement(
+                      "script"
+                    );
+
+                  script.type =
+                    "text/javascript";
+
+                  script.async = true;
+
+                  script.src =
+                    url +
+                    "?sdkid=" +
+                    pixelId +
+                    "&lib=" +
+                    t;
+
+                  var firstScript =
+                    document.getElementsByTagName(
+                      "script"
+                    )[0];
+
+                  firstScript.parentNode.insertBefore(
+                    script,
+                    firstScript
+                  );
+                };
+
+                ttq.load(
+                  "DA46CLRC77U4BICJ0VF0"
+                );
+
+                ttq.page();
+              }(
+                window,
+                document,
+                "ttq"
+              );
+            `,
+          }}
+        />
       </head>
 
       <body
